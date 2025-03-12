c4bdce1ade665825723ba33ecba084db2669650d
Chapstikc <76835543+Chapstikc@users.noreply.github.com>
2025-03-12 21:28:02 +0000

    Update content.js
    Customized the search parameters and inserted text of the extension

--- content.js
+++ content.js
@@ -1,8 +1,9 @@
 // content.js
-const textToInsert = "Hello World";
+const textToInsert = "UPO_SHIPPED_FROM - University Post Office (WSC)"; // Text to be inserted into targeted box
 
 // Find the input box by its ID, name, or any other selector
-const inputBox = document.querySelector('input[type="text"]'); // Adjust the selector as needed
+const inputBox = document.querySelector('input[placeholder="Business Unit"]'); // Targets the box with the placeholder text "Business Unit"
+
 
 if (inputBox) {
   inputBox.value = textToInsert;
